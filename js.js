88f7e78b3c03f0eb1f333bfb0134ddafc908265b
Friso Buurman <frisobuurman@hotmail.com>
2017-12-07 13:08:36 +0100

    First comment section live test

--- js.js
+++ js.js
@@ -2,6 +2,39 @@
 $( "html").hide().fadeIn(1000);
 $( ".socialNetworkNav" ).hide().fadeIn(2000);
 
+displayComments();
+
+function submitComment() {
+  var uname = $( "#uname" ).val();
+  var ctext = $( "#ctext" ).val();
+
+  var d = new Date();
+  var cdate = d.toUTCString();
+
+  var curComments = getCookie("comments");
+  newComments = curComments + uname + "|" + ctext + "|" + cdate + ";"
+
+  console.log(newComments);
+
+  setCookie("comments", newComments);
+}
+
+function displayComments() {
+  var comments = getCookie("comments");
+  if ( !comments ) {
+    comments = "Friso Buurman|Initial comment|Fri, 01 Dec 2017 11:57:03 GMT;"
+  }
+
+  $( ".commentSection ol li" ).remove();
+
+  var commentsArray = comments.split(';');
+  for(var i = 0; i < commentsArray.length - 1; i++) {
+    commentArray = commentsArray[i].split('|');
+    $( ".commentSection ol" ).append("<li><article class=\"comment\"><div class=\"entry-content\"><p>" + commentArray[1] + "</p></div><footer class=\"post-info\"><abbr class=\"published\" title=\"" + commentArray[2] + "\">" + commentArray[2] + "</abbr><address class=\"vcard author\">By <a class=\"url fn\" href=\"#\">" + commentArray[0] + "</a></address></footer></article></li>");
+  }
+}
+
+
 //manage cookies
 var cname = "useCookies";
 if ( getCookie(cname) ) {
@@ -22,13 +55,16 @@ function cookieNo() {
   removeCookieBar();
 }
 
+//general cookie functions
+function setCookie(cname, cvalue) {
+    setCookie(cname, cvalue, 31);
+}
+
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+ d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-    console.log(cname + "=" + cvalue + ";" + expires + ";path=/");
-
 }
 
 function getCookie(cname) {
